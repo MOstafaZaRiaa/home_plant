3ef6ed14837668f545acc042781b1c7f78b3a5a2
Mostafa Ahmed <40537665+MOstafaZaRiaa@users.noreply.github.com>
2021-01-29 01:17:56 +0200

    Update poduct_item.dart

--- lib/widgts/poduct_item.dart
+++ lib/widgts/poduct_item.dart
@@ -15,72 +15,78 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final deviceHeight = MediaQuery.of(context).size.height;
     final deviceWidth = MediaQuery.of(context).size.width;
-    return Container(
-      padding: EdgeInsets.all(deviceWidth * 0.02),
-      child: Stack(
-        children: [
-          Container(
-            height: deviceHeight * 0.25,
-            width: deviceWidth * 0.3,
-            decoration: BoxDecoration(
-                color: Theme.of(context).backgroundColor,
-                borderRadius: BorderRadius.circular(20)),
-          ),
-          Container(
-              height: deviceHeight * 0.155,
+    return PhysicalModel(
+      elevation: 8.0,
+      borderRadius: BorderRadius.circular(20),
+      color: Theme.of(context).backgroundColor,
+      child: Container(
+        padding: EdgeInsets.all(deviceWidth * 0.03),
+
+        child: Stack(
+          children: [
+            Container(
+              height: deviceHeight * 0.25,
               width: deviceWidth * 0.3,
               decoration: BoxDecoration(
-                  color: Theme.of(context).accentColor,
+                  color: Theme.of(context).backgroundColor,
                   borderRadius: BorderRadius.circular(20)),
-              child: FadeInImage(
-                fit: BoxFit.fill,
-                image: NetworkImage(
-                  productImage,
-                ),
-                placeholder: AssetImage('assets/images/plant_outline_dark.png'),
-              ),
-              ),
-          Positioned(
-            left: deviceWidth * .02,
-            bottom: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      productName,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      '$productPrice\$',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  width: deviceWidth * 0.06,
-                ),
-                IconButton(
-                    icon: Icon(Icons.add_circle_rounded), onPressed: () {}),
-              ],
             ),
-          ),
-          Positioned(
-            right: 0,
-            top: 0,
-            child: IconButton(
-              icon: Icon(
-                Icons.favorite_border,
-                color: Theme.of(context).accentTextTheme.headline1.color,
+            Container(
+                height: deviceHeight * 0.155,
+                width: deviceWidth * 0.3,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).accentColor,
+                    borderRadius: BorderRadius.circular(20)),
+                child: FadeInImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                    productImage,
+                  ),
+                  placeholder: AssetImage('assets/images/plant_outline_dark.png'),
+                ),
+                ),
+            Positioned(
+              left: deviceWidth * .02,
+              bottom: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        productName,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '$productPrice\$',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: deviceWidth * 0.06,
+                  ),
+                  IconButton(
+                      icon: Icon(Icons.add_circle_rounded), onPressed: () {}),
+                ],
               ),
-              onPressed: () {
-                bool isFavorite = false;
-              },
             ),
-          ),
-        ],
+            Positioned(
+              right: 0,
+              top: 0,
+              child: IconButton(
+                icon: Icon(
+                  Icons.favorite_border,
+                  color: Theme.of(context).accentTextTheme.headline1.color,
+                ),
+                onPressed: () {
+                  bool isFavorite = false;
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
