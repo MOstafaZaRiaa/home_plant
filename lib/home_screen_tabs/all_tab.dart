86cc4f21c14e291726864a5b295e222e5eaada31
Mostafa Ahmed <40537665+MOstafaZaRiaa@users.noreply.github.com>
2021-01-29 06:06:14 +0200

    Update all_tab.dart

--- lib/home_screen_tabs/all_tab.dart
+++ lib/home_screen_tabs/all_tab.dart
@@ -10,197 +10,133 @@ class AllTab extends StatelessWidget {
     final deviceHeight = MediaQuery.of(context).size.height;
     final deviceWidth = MediaQuery.of(context).size.width;
     return SingleChildScrollView(
-      child:Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                StreamBuilder(
-                  stream:FirebaseFirestore.instance.collection('products').where('productKind',
-                    isEqualTo: 'Plant',).limit(15).snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    final products = snapshot.data.docs;
-                    return Container(
-                      height: deviceHeight * 0.31,
-                      margin: EdgeInsets.all(deviceWidth * 0.01),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Plants',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              TextButton(onPressed: (){}, child: Text('See more >',style: TextStyle(color: Theme.of(context).textTheme.headline1.color,),),),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          RowOfDifferentProducts(
+            deviceHeight: deviceHeight,
+            deviceWidth: deviceWidth,
+            stream: FirebaseFirestore.instance
+                .collection('products')
+                .where(
+                  'productKind',
+                  isEqualTo: 'Plant',
+                )
+                .limit(15)
+                .snapshots(),
+          ),
+          RowOfDifferentProducts(
+            deviceHeight: deviceHeight,
+            deviceWidth: deviceWidth,
+            stream: FirebaseFirestore.instance
+                .collection('products')
+                .where(
+                  'productKind',
+                  isEqualTo: 'Flowers',
+                )
+                .limit(15)
+                .snapshots(),
+          ),
+          RowOfDifferentProducts(
+            deviceHeight: deviceHeight,
+            deviceWidth: deviceWidth,
+            stream: FirebaseFirestore.instance
+                .collection('products')
+                .where(
+                  'productKind',
+                  isEqualTo: 'Accessories',
+                )
+                .limit(15)
+                .snapshots(),
+          ),
+          RowOfDifferentProducts(
+            deviceHeight: deviceHeight,
+            deviceWidth: deviceWidth,
+            stream: FirebaseFirestore.instance
+                .collection('products')
+                .where(
+                  'productKind',
+                  isEqualTo: 'Pot',
+                )
+                .limit(15)
+                .snapshots(),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-                            ],
-                          ),
-                          Container(
-                            height: deviceHeight * 0.24,
-                            width: deviceWidth * 0.3,
-                            child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: products.length,
-                              itemBuilder: (BuildContext context, int index) =>
-                                  ProductItem(
-                                productName: products[index]['productName'],
-                                productPrice: products[index]['productPrice'],
-                                productImage: products[index]['image_url'],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                ),
-                StreamBuilder(
-                  stream:FirebaseFirestore.instance.collection('products').where('productKind',
-                    isEqualTo: 'Flowers',).limit(15).snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    final products = snapshot.data.docs;
-                    return Container(
-                      height: deviceHeight * 0.31,
-                      margin: EdgeInsets.all(deviceWidth * 0.01),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Flowers',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              TextButton(onPressed: (){}, child: Text('See more >',style: TextStyle(color: Theme.of(context).textTheme.headline1.color,),),),
-                            ],
-                          ),
-                          Container(
-                            height: deviceHeight * 0.24,
-                            width: deviceWidth * 0.3,
-                            child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: products.length,
-                              itemBuilder: (BuildContext context, int index) =>
-                                  ProductItem(
-                                productName: products[index]['productName'],
-                                productPrice: products[index]['productPrice'],
-                                productImage: products[index]['image_url'],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                ),
-                StreamBuilder(
-                  stream:FirebaseFirestore.instance.collection('products').where('productKind',
-                    isEqualTo: 'Accessories',).limit(15).snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    final products = snapshot.data.docs;
-                    return Container(
-                      height: deviceHeight * 0.31,
-                      margin: EdgeInsets.all(deviceWidth * 0.01),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Accessories',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              TextButton(onPressed: (){}, child: Text('See more >',style: TextStyle(color: Theme.of(context).textTheme.headline1.color,),),),
-                            ],
-                          ),
-                          Container(
-                            height: deviceHeight * 0.24,
-                            width: deviceWidth * 0.3,
-                            child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: products.length,
-                              itemBuilder: (BuildContext context, int index) =>
-                                  ProductItem(
-                                productName: products[index]['productName'],
-                                productPrice: products[index]['productPrice'],
-                                productImage: products[index]['image_url'],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                ),
-                StreamBuilder(
-                  stream:FirebaseFirestore.instance.collection('products').where('productKind',
-                    isEqualTo: 'Pot',).limit(15).snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    final products = snapshot.data.docs;
-                    return Container(
-                      height: deviceHeight * 0.31,
-                      margin: EdgeInsets.all(deviceWidth * 0.01),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Pot',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              TextButton(onPressed: (){}, child: Text('See more >',style: TextStyle(color: Theme.of(context).textTheme.headline1.color,),),),
-                            ],
-                          ),
-                          Container(
-                            height: deviceHeight * 0.24,
-                            width: deviceWidth * 0.3,
-                            child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: products.length,
-                              itemBuilder: (BuildContext context, int index) =>
-                                  ProductItem(
-                                productName: products[index]['productName'],
-                                productPrice: products[index]['productPrice'],
-                                productImage: products[index]['image_url'],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                ),
-              ],
-            ),
+class RowOfDifferentProducts extends StatelessWidget {
+  const RowOfDifferentProducts(
+      {Key key,
+      @required this.deviceHeight,
+      @required this.deviceWidth,
+      @required this.stream})
+      : super(key: key);
 
+  final double deviceHeight;
+  final double deviceWidth;
+  final Stream stream;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: deviceHeight * 0.32,
+      child: StreamBuilder(
+          stream:
+              stream, //FirebaseFirestore.instance.collection('products').where('productKind',
+          //isEqualTo: 'Plant',).limit(15).snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final products = snapshot.data.docs;
+            return Container(
+              height: deviceHeight * 0.31,
+              margin: EdgeInsets.all(deviceWidth * 0.01),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Plants',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'See more >',
+                          style: TextStyle(
+                            color: Theme.of(context).textTheme.headline1.color,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: deviceHeight * 0.24,
+                    width: deviceWidth * 0.3,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: products.length,
+                      itemBuilder: (BuildContext context, int index) =>
+                          ProductItem(
+                        productName: products[index]['productName'],
+                        productPrice: products[index]['productPrice'],
+                        productImage: products[index]['image_url'],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }),
     );
   }
 }
