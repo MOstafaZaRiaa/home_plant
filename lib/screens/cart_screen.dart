2c173a77619abe74db9163b92c324965b95cae6f
Mostafa Ahmed <40537665+MOstafaZaRiaa@users.noreply.github.com>
2021-02-04 04:05:54 +0200

    Update cart_screen.dart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:home_plant/screens/check_out_screen.dart';
 import 'package:home_plant/widgts/product_item_in_cart.dart';
 
 class CartScreen extends StatefulWidget {
@@ -16,10 +17,7 @@ class _CartScreenState extends State<CartScreen> {
 
   User user = FirebaseAuth.instance.currentUser;
 
-
-
   Widget build(BuildContext context) {
-    total = 0;
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -27,56 +25,79 @@ class _CartScreenState extends State<CartScreen> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: StreamBuilder(
-              stream: FirebaseFirestore.instance
-                  .collection('users')
-                  .doc(user.uid)
-                  .collection('cart').snapshots(),
-              builder: (context, snapshot) {
-                if(snapshot.connectionState == ConnectionState.waiting){
-                  return Center(child: CircularProgressIndicator(),);
-                }
-                final products = snapshot.data.docs;
-                length = products.length;
-                return ListView.builder(
-                  itemCount: products.length,
-                  itemBuilder: (BuildContext context, int index){
-                    total = total + products[index]['totalPrice'];
-                    print(total);
-                    return ProductItemInCart(
-                      productName: products[index]['productName'],
-                      productPrice: products[index]['productPrice'],
-                      productImagePath: products[index]['imagePath'],
-                      productId: products[index]['productId'],
-                      user :user,
-                      totalPrice: products[index]['totalPrice'],
-                      productAmount:  products[index]['productAmount'],
-                    );
-                  },
-                );
-              }
-            ),
-          ),
-          InkWell(
-            child: FlatButton(
-              padding: EdgeInsets.all(MediaQuery.of(context).size.height*0.024),
-              child: Text(
-                'PROCEED',
-                style: TextStyle(
-                    color: Theme.of(context).accentTextTheme.headline1.color),
-              ),
-              onPressed: () {
-
-              },
-              color: Theme.of(context).primaryColor,
-            ),
-          ),
-        ],
-      ),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .doc(user.uid)
+              .collection('cart')
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final products = snapshot.data.docs;
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: products.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ProductItemInCart(
+                        productName: products[index]['productName'],
+                        productPrice: products[index]['productPrice'],
+                        productImagePath: products[index]['imagePath'],
+                        productId: products[index]['productId'],
+                        user: user,
+                        totalPrice: products[index]['totalPrice'],
+                        productAmount: products[index]['productAmount'],
+                      );
+                    },
+                  ),
+                ),
+                InkWell(
+                  child: FlatButton(
+                    padding: EdgeInsets.all(
+                        MediaQuery.of(context).size.height * 0.024),
+                    child: Text(
+                      'PROCEED',
+                      style: TextStyle(
+                          color: Theme.of(context)
+                              .accentTextTheme
+                              .headline1
+                              .color),
+                    ),
+                    onPressed: products.length == 0
+                        ? () {}
+                        : () async {
+                      total = 0;
+                            await FirebaseFirestore.instance
+                                .collection('users')
+                                .doc(user.uid)
+                                .collection('cart')
+                                .get()
+                                .then((QuerySnapshot querySnapshot) => {
+                                      querySnapshot.docs.forEach((doc) {
+                                        total = total + doc['totalPrice'] ;
+                                      }),
+                                    });
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => CheckOutScreen(
+                                  int: total,
+                                ),
+                              ),
+                            );
+                          },
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ],
+            );
+          }),
     );
   }
 }
